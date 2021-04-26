02ea28fef31e43cac2c2ffc98b848b846854bd5e
Deliveos <deliveosdars@gmail.com>
2021-04-26 16:15:05 +0600

    Add dynamic

---
+++ scripts/logo.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar1").style.width = scrolled + "%";
+  document.getElementById("myBar2").style.width = scrolled + "%";
+} 
